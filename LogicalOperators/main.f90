1427674713635fe32214f6c453baae88dcdca06e
A3Tom <Tom_Esq@Yahoo.com>
2020-03-27 01:15:45 +0000

    Added Logical Operators

---
+++ LogicalOperators/main.f90
@@ -0,0 +1,10 @@
+program logicaloperatiors
+    implicit none   
+
+    ! This looks like a laugh but it's real annoying to type out
+    print *, .true. .or. .false. 
+    print *, .not. .true.
+    print *, 5 /= 9
+    print *, "a" < "b"
+
+end program logicaloperatiors
